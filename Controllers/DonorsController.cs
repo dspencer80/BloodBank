a6817e7c4713433b3cd9888e31d2e51d2f5d9225
dspencer80 <d.a.spencer@live.com>
2022-07-29 00:08:19 -0400

    adding option to update donors

--- Controllers/DonorsController.cs
+++ Controllers/DonorsController.cs
@@ -9,6 +9,7 @@ namespace BloodBank.Controllers
     public class DonorsController : Controller
     {
         private readonly BloodBankDbContext bloodBankDbContext;
+        
 
         public DonorsController(BloodBankDbContext bloodBankDbContext)
         {
@@ -50,6 +51,53 @@ namespace BloodBank.Controllers
 
         }
 
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var donor = await bloodBankDbContext.Donors.FirstOrDefaultAsync(x => x.Id == id);
 
+            if (donor != null)
+            {
+                var viewModel = new UpdateDonorViewModel()
+                {
+                    Id = donor.Id,
+                    FirstName = donor.FirstName,
+                    LastName = donor.LastName,
+                    Social = donor.Social,
+                    Email = donor.Email,
+                    Race = donor.Race,
+                    DateofBirth = donor?.DateofBirth,
+                    DonationDate = donor?.DonationDate,
+                };
+
+                return await Task.Run(() =>View("view", viewModel));
+            }
+
+            
+                return RedirectToAction("Index");
+        }
+
+           [HttpPost]
+           public async Task<IActionResult> View(UpdateDonorViewModel model)
+        {
+            var donor = await bloodBankDbContext.Donors.FindAsync(model.Id);
+
+            if (donor != null)
+            {
+                donor.FirstName = model.FirstName;
+                donor.LastName = model.LastName;
+                donor.Social = model.Social;    
+                donor.Email = model.Email;
+                donor.Race = model.Race;
+                donor.DateofBirth = model.DateofBirth;
+                donor.DonationDate = model.DonationDate;
+
+                await bloodBankDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Models/UpdateDonorViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BloodBank.Models
+{
+    public class UpdateDonorViewModel
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? Social { get; set; }
+        public string? Email { get; set; }
+        public string? Race { get; set; }
+        public DateTime? DateofBirth { get; set; }
+        public DateTime? DonationDate { get; set; }
+
+    }
+}
